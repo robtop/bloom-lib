2a06dc35183d0ed48b0f4b30d3fd49abc02e5fe8
kalle.hamalainen85@gmail.com <kalle.hamalainen85@gmail.com@c2ef9785-a31d-bad2-a4c3-f09bd6b9b790>
2012-03-29 19:23:22 +0000

    Initial commit

---
+++ trunk/BloomLib/src/testProject/DesktopLauncher.java
@@ -0,0 +1,22 @@
+package testProject;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class DesktopLauncher {
+
+	public static void main(String[] argv) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "curling";
+		config.width = 800;
+		config.height = 480;
+		config.samples = 2;
+		config.vSyncEnabled = false;
+		config.useCPUSynch = false;
+		config.useGL20 = true;
+
+		config.fullscreen = false;
+		new LwjglApplication(new GdxTest(), config);
+	}
+
+}
